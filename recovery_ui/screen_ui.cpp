sileshn/android_bootable_recovery

commit bb66cb78b791016e1225061bed547dd8bf1b1c8a

recovery_ui: Adjust menu items style
Should be less confusing to users and stop them from thinking that
header text is a clickable option.

Change-Id: Iffa513855a1cfe82afcf8b3dda447bd542502f8d

recovery_ui/screen_ui.cpp
@@ -183,35 +183,37 @@ int TextMenu::DrawHeader(int x, int y) const {
   return offset;
 }
 
-int TextMenu::DrawItems(int x, int y, int screen_width, bool long_press) const {
+int TextMenu::DrawItems(int /*x*/, int y, int screen_width, bool long_press) const {
+  int horizontal_rule_height = 8;
   int offset = 0;
   int padding = draw_funcs_.MenuItemPadding();
+  int spacing = draw_funcs_.MenuItemSpacing();
 
   draw_funcs_.SetColor(UIElement::MENU);
-  offset += draw_funcs_.DrawHorizontalRule(y + offset) + 4;
+  offset += horizontal_rule_height + 4;
 
   int item_container_offset = offset; // store it for drawing scrollbar on most top
 
   for (size_t i = MenuStart(); i < MenuEnd(); ++i) {
-    if (i == selection()) {
-      // Draw the highlight bar.
-      draw_funcs_.SetColor(long_press ? UIElement::MENU_SEL_BG_ACTIVE : UIElement::MENU_SEL_BG);
+    const auto selected = i == selection();
 
-      int bar_height = padding + char_height_ + padding;
-      draw_funcs_.DrawHighlightBar(0, y + offset, screen_width, bar_height);
+    // Draw the highlight bar.
+    draw_funcs_.SetColor(long_press ? UIElement::MENU_SEL_BG_ACTIVE
+                         : selected ? UIElement::MENU_SEL_BG
+                                    : UIElement::MENU_BG);
 
-      // Colored text for the selected item.
-      draw_funcs_.SetColor(UIElement::MENU_SEL_FG);
-    }
-    offset += draw_funcs_.DrawTextLine(x, y + offset, TextItem(i), false /* bold */);
+    int bar_height = padding + char_height_ + padding;
+    draw_funcs_.DrawHighlightBar(padding, y + offset, screen_width - (padding * 2), bar_height);
 
-    draw_funcs_.SetColor(UIElement::MENU);
+    draw_funcs_.SetColor(selected ? UIElement::MENU_SEL_FG : UIElement::MENU);
+    offset += draw_funcs_.DrawTextLine(padding * 2, y + offset, TextItem(i), false /* bold */);
+    offset += spacing;
   }
-  offset += draw_funcs_.DrawHorizontalRule(y + offset);
+  offset += horizontal_rule_height;
 
   std::string unused;
   if (ItemsOverflow(&unused)) {
-    int container_height = max_display_items_ * (2 * padding + char_height_);
+    int container_height = max_display_items_ * (2 * padding + char_height_ + spacing);
     int bar_height = container_height / (text_items_.size() - max_display_items_ + 1);
     int start_y = y + item_container_offset + bar_height * menu_start_;
     draw_funcs_.SetColor(UIElement::SCROLLBAR);
@@ -606,6 +608,12 @@ void ScreenRecoveryUI::SetColor(UIElement e) const {
     case UIElement::MENU:
       gr_color(0xd8, 0xd8, 0xd8, 255);
       break;
+    case UIElement::MENU_BG:
+      if (fastbootd_logo_enabled_)
+        gr_color(0xe6 * 0.20, 0x51 * 0.20, 0x00 * 0.20, 255);
+      else
+        gr_color(0x7c * 0.20, 0x4d * 0.20, 0xff * 0.20, 255);
+      break;
     case UIElement::MENU_SEL_BG:
     case UIElement::SCROLLBAR:
       if (fastbootd_logo_enabled_)
@@ -863,7 +871,8 @@ void ScreenRecoveryUI::draw_menu_and_text_buffer_locked(
     menu_->SetMenuHeight(std::max(0, ScreenHeight() - menu_start_y_));
     y += menu_->DrawItems(x, y, ScreenWidth(), IsLongPress());
     if (!help_message.empty()) {
-      y += MenuItemPadding();
+      y -= MenuItemSpacing();
+      y += 12; // Skip horizontal rule and some margin
       SetColor(UIElement::INFO);
       y += DrawTextLines(x, y, help_message);
     }
@@ -1547,10 +1556,18 @@ int ScreenRecoveryUI::SelectMenu(const Point& p) {
       }
     }
 
+    const int menu_item_height = MenuItemHeight();
+    const int menu_item_height_with_spacing = menu_item_height + MenuItemSpacing();
+
     if (point.y() >= menu_start_y_ &&
-        point.y() < menu_start_y_ + menu_->ItemsCount() * MenuItemHeight()) {
+        point.y() < menu_start_y_ + menu_->ItemsCount() * menu_item_height_with_spacing) {
       int old_sel = menu_->selection();
-      int relative_sel = (point.y() - menu_start_y_) / MenuItemHeight();
+      int relative_sel = (point.y() - menu_start_y_) / menu_item_height_with_spacing;
+      int menu_item_start_y = menu_start_y_ + (relative_sel * menu_item_height_with_spacing);
+      if (point.y() > menu_item_start_y + menu_item_height) {
+        // The touch is in the spacing area between two menu items.
+        return Device::kNoAction;
+      }
       new_sel = menu_->SelectVisible(relative_sel);
       if (new_sel != -1 && new_sel != old_sel) {
         update_screen_locked();

recovery_ui/include/recovery_ui/screen_ui.h
@@ -35,6 +35,7 @@ enum class UIElement {
   BATTERY_LOW,
   HEADER,
   MENU,
+  MENU_BG,
   MENU_SEL_BG,
   MENU_SEL_BG_ACTIVE,
   MENU_SEL_FG,
@@ -85,6 +86,7 @@ class DrawInterface {
   virtual int MenuCharHeight() const = 0;
   virtual int MenuCharWidth() const = 0;
   virtual int MenuItemPadding() const = 0;
+  virtual int MenuItemSpacing() const = 0;
   virtual int MenuItemHeight() const = 0;
 };
 
@@ -270,6 +272,9 @@ class MenuDrawFunctions : public DrawInterface {
   int MenuItemPadding() const override {
     return wrappee_.MenuItemPadding();
   };
+  int MenuItemSpacing() const override {
+    return wrappee_.MenuItemSpacing();
+  };
   int MenuItemHeight() const override {
     return wrappee_.MenuItemHeight();
   };
@@ -341,6 +346,10 @@ class ScreenRecoveryUI : public RecoveryUI, public DrawInterface {
 
   int MenuItemHeight() const override {
     return MenuCharHeight() + 2 * MenuItemPadding();
+  }
+
+  int MenuItemSpacing() const override {
+    return 8;
   }
 
  protected:

commit acd99f7c48e0229eaedd40aa3e2adcbabe2dc283

recoveryui: Apply margin_width_ when drawing menu items
On smith the display is defined as 1080x2142 in
the dt, but the resolution is actually 876x2142.
Stock recovery fixes this with a horizontal
margin.

Everything else already respects the margin, this
just fixes the menu items.

Fixes: bb66cb78b791016e1225061bed547dd8bf1b1c8a
Change-Id: If8d38159354253a13ad3eb25e6296f8910c65060

recovery_ui/screen_ui.cpp
@@ -183,7 +183,7 @@ int TextMenu::DrawHeader(int x, int y) const {
   return offset;
 }
 
-int TextMenu::DrawItems(int /*x*/, int y, int screen_width, bool long_press) const {
+int TextMenu::DrawItems(int x, int y, int screen_width, bool long_press) const {
   int horizontal_rule_height = 8;
   int offset = 0;
   int padding = draw_funcs_.MenuItemPadding();
@@ -203,10 +203,10 @@ int TextMenu::DrawItems(int /*x*/, int y, int screen_width, bool long_press) con
                                     : UIElement::MENU_BG);
 
     int bar_height = padding + char_height_ + padding;
-    draw_funcs_.DrawHighlightBar(padding, y + offset, screen_width - (padding * 2), bar_height);
+    draw_funcs_.DrawHighlightBar(padding + x, y + offset, screen_width - (padding + x) * 2, bar_height);
 
     draw_funcs_.SetColor(selected ? UIElement::MENU_SEL_FG : UIElement::MENU);
-    offset += draw_funcs_.DrawTextLine(padding * 2, y + offset, TextItem(i), false /* bold */);
+    offset += draw_funcs_.DrawTextLine(padding * 2 + x, y + offset, TextItem(i), false /* bold */);
     offset += spacing;
   }
   offset += horizontal_rule_height;
@@ -855,7 +855,7 @@ void ScreenRecoveryUI::draw_menu_and_text_buffer_locked(
     if (!menu_->IsMain()) {
       auto icon_w = gr_get_width(back_icon_.get());
       auto icon_h = gr_get_height(back_icon_.get());
-      auto icon_x = centered_x / 2 - icon_w / 2;
+      auto icon_x = (centered_x - margin_width_) / 2 - icon_w / 2 + margin_width_;
       auto icon_y = y - logo_height / 2 - icon_h / 2;
       gr_blit(back_icon_sel_ && menu_->selection() == -1 ? back_icon_sel_.get() : back_icon_.get(),
               0, 0, icon_w, icon_h, icon_x, icon_y);